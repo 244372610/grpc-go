555cb0da75184d9973e38e6c518f37a3f21637e8
sunwp <244372610@qq.com>
2021-12-22 00:47:29 +0800

    resolver
    Signed-off-by: sunwp <244372610@qq.com>

--- resolver_conn_wrapper.go
+++ resolver_conn_wrapper.go
@@ -33,6 +33,7 @@ import (
 
 // ccResolverWrapper is a wrapper on top of cc for resolvers.
 // It implements resolver.ClientConn interface.
+// ccResolverWrapper 是对 ClientConn 的封装
 type ccResolverWrapper struct {
 	cc         *ClientConn
 	resolverMu sync.Mutex
